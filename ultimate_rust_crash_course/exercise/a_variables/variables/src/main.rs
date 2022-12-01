b5d053548e1a8a922fe9c1d83886775bdddaae69
Dennis <drkennetz@gmail.com>
2022-11-30 18:58:51 -0600

    solved the function challenge and added a library

--- ultimate_rust_crash_course/exercise/a_variables/variables/src/main.rs
+++ ultimate_rust_crash_course/exercise/a_variables/variables/src/main.rs
@@ -5,7 +5,7 @@ const READY_AMOUNT: i32 = 2;
 fn main() {
     //let mut missiles: i32 = STARTING_MISSILES;
     //let ready: i32 = READY_AMOUNT;
-    let (mut missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
+    let (mut missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
     println!("Firing {ready} of my {missiles} missiles");
     missiles = missiles - ready;
     println!("{missiles} missiles left");
